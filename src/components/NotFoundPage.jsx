1ee039952d8246ebd4107f5aa6a6ffd8e051550d
Michael Allen <mike@mistColoured.com>
2018-01-09 11:10:13 +0000

    Branching

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => (
-  <div>404! - <Link to="/">Go Home</Link></div>
+  <div>404! - <Link to="/">Go Home Inside Master</Link></div>
 );
 
 export default NotFoundPage;
